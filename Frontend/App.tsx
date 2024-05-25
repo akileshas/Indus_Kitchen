a2064130aeae704f3adac9747b6d7c23c9bd88f0
Akilesh-ML-Engineer <akilesh.ml.engineer@gmail.com>
2024-05-25 06:26:35 +0530

    Completed the Scroll to Top functionality!

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/navbar/navbar';
 import Home from './Components/home/home';
 import About from './Components/about/about';
 import Choose from './Components/choose/choose';
-
+import Story from './Components/story/story';
+import Menu from './Components/menu/menu';
 import Testimonial from './Components/testimonial/testimonial';
 import Form from './Components/form/form';
 import Contact from './Components/contact/contact';
@@ -14,6 +15,30 @@ import SmallScreen from './Components/small-screen/small-screen';
 import './App.css';
 
 function App() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 130) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 130) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  }, [showScroll]);
+
   return (
     <>
       <div className='App'>
@@ -21,7 +46,8 @@ function App() {
         <Home />
         <About />
         <Choose />
-
+        <Story />
+        <Menu />
         <Testimonial />
         <Form />
         <Contact />
@@ -30,8 +56,16 @@ function App() {
         <Footer />
         <SmallScreen />
       </div>
+      <button
+        id="ScrollToTopBtn"
+        className={showScroll ? 'show' : ''}
+        onClick={scrollToTop}
+        title="Go to top"
+      >
+        <i className='bx bxs-chevron-up'></i>
+      </button>
     </>
-  )
-};
+  );
+}
 
 export default App;
---
+++ Frontend/Components/story/story.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './story.css';
+
+const Story = () => {
+    return (
+        <div id="our-story">
+            
+        </div>
+    );
+};
+
+export default Story;
